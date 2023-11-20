9ebc4b61aa74a88966f182b0e567f2817681a0b0
Elena Gerasimovich <nararock@mail.ru>
2023-11-20 22:21:24 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿void getMultTable(int num)
+{
+    for (int i = 1; i <= 9; i++)
+    {
+        Console.WriteLine($"{num} * {i} = {num * i}");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+getMultTable(number);
